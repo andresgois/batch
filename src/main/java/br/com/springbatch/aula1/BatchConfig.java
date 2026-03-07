6a54d5bfda06ba20ff16d6224ad032cd5a5f8c9a
andre <andresousalee@gmail.com>
2026-03-07 20:46:31 -0300

    aula 1 funcionando corretamente

--- src/main/java/br/com/springbatch/aula1/BatchConfig.java
+++ src/main/java/br/com/springbatch/aula1/BatchConfig.java
@@ -1,35 +1,28 @@
 package br.com.springbatch.aula1;
 
-
 import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.job.builder.JobBuilder;
-import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.repeat.RepeatStatus;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
 
-import jakarta.annotation.PostConstruct;
+//@EnableBatchProcessing
 
-@EnableBatchProcessing      // para trazer todas as features do spring batch, é necessário habilita-lo
-@Configuration              // necessário ser uma classe de configuração
+/**
+ * 1. Remova @EnableBatchProcessing
+ *
+ * No Spring Batch 5 com Spring Boot 3, o @EnableBatchProcessing não é mais necessário e pode causar conflitos. O Spring Boot configura automaticamente o batch quando detecta a dependência.
+ */
+@Configuration
 public class BatchConfig {
 
-    @Autowired
-    private JobLauncher jobLauncher;
-    
-    @Autowired 
-    private Job imprimirOlaJob;
-
-    @Bean                   // retorna um job
+    @Bean
     public Job imprimirOlaJob(JobRepository jobRepository, Step stepOlaMundo) {
-        return new JobBuilder("imprimirOlaJob", jobRepository) // nome do job , o job é dividido em etapas que são chamado de steps, steps são encadeados para contér um lógica maior
+        return new JobBuilder("imprimirOlaJob", jobRepository)
                 .start(stepOlaMundo)
                 .build();
     }
@@ -38,12 +31,11 @@ public class BatchConfig {
     public Step stepOlaMundo(JobRepository jobRepository,
                              PlatformTransactionManager transactionManager) {
 
-        return new StepBuilder("stepOlaMundo", jobRepository)// Nome do step
+        return new StepBuilder("stepOlaMundo", jobRepository)
                 .tasklet((contribution, chunkContext) -> {
                     System.out.println("Olá Mundo");
                     return RepeatStatus.FINISHED;
                 }, transactionManager)
                 .build();
     }
-
 }
